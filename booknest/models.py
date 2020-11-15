79b2152a71432ae8facf423f1eb4bb40765231dd
ewijohnson <ericwj2@illinois.edu>
2020-11-15 11:21:18 -0600

    updated null=True and defaults for optional foreign keys

--- booknest/models.py
+++ booknest/models.py
@@ -9,38 +9,45 @@ class MainEntry(models.Model):
     main_entry_identifier = models.CharField(max_length=255, blank=True, default='')
     main_entry_role = models.CharField(max_length=255)
 
+    def __str__(self):
+        result = ''
+        if self.main_entry_date != '':
+            if self.main_entry_identifier != '':
+                result = '%s, %s (%s)' % (self.main_entry_name, self.main_entry_date, self.main_entry_identifier)
+            else:
+                result = '%s, %s' % (self.main_entry_name, self.main_entry_date)
+        else:
+            if self.main_entry_identifier != '':
+                result = '%s (%s)' % (self.main_entry_name, self.main_entry_identifier)
+            else:
+                result = '%s' % self.main_entry_name
+        return result
+
     class Meta:
         verbose_name = 'Main Entry'
         verbose_name_plural = 'Main Entries'
         unique_together = ('main_entry_name', 'main_entry_date', 'main_entry_identifier', 'main_entry_role')
-
-    def __str__(self):
-        if self.main_entry_date != '':
-            if self.main_entry_identifier != '':
-                return '%s, %s (%s)' % (self.main_entry_name, self.main_entry_date, self.main_entry_identifier)
-            else:
-                return '%s, %s' % (self.main_entry_name, self.main_entry_date)
-        else:
-            if self.main_entry_identifier != '':
-                return '%s (%s)' % (self.main_entry_name, self.main_entry_identifier)
-            else:
-                return '%s' % self.main_entry_name
+        ordering = ['main_entry_name', 'main_entry_date', 'main_entry_identifier']
 
 
 class Work(models.Model):
     work_id = models.AutoField(primary_key=True)
     work_title = models.CharField(max_length=255)
     work_title_var = models.CharField(max_length=255, blank=True, default='')
-    main_entry = models.ForeignKey(MainEntry, related_name='works', blank=True, default='', on_delete=models.PROTECT)
+    main_entry = models.ForeignKey(MainEntry, related_name='works', null=True, blank=True, default=None,
+                                   on_delete=models.PROTECT)
+
+    def __str__(self):
+        result = ''
+        if self.main_entry != '':
+            result = '%s (%s)' % (self.work_title, self.main_entry)
+        else:
+            result = '%s' % self.work_title
+        return result
 
     class Meta:
         unique_together = ('work_title', 'main_entry')
-
-    def __str__(self):
-        if self.main_entry != '':
-            return '%s (%s)' % (self.work_title, self.main_entry)
-        else:
-            return '%s' % self.work_title
+        ordering = ['work_title', 'main_entry']
 
 
 class Series(models.Model):
@@ -52,25 +59,28 @@ class Series(models.Model):
     series_author_identifier = models.CharField(max_length=255, blank=True, default='')
     series_issn = models.CharField(max_length=9, blank=True, default='')
 
-    class Meta:
-        verbose_name_plural = 'Series'
-
     def __str__(self):
+        result = ''
         if self.series_date != '':
             if self.series_author != '':
                 if self.series_author_date != '':
                     if self.series_author_identifier != '':
-                        return '%s, %s (%s, %s (%s))' % (self.series_title, self.series_date, self.series_author,
-                                                         self.series_author_date, self.series_author_identifier)
+                        result = '%s, %s (%s, %s (%s))' % (self.series_title, self.series_date, self.series_author,
+                                                           self.series_author_date, self.series_author_identifier)
                     else:
-                        return '%s, %s (%s, %s)' % (self.series_title, self.series_date, self.series_author,
-                                                    self.series_author_date)
+                        result = '%s, %s (%s, %s)' % (self.series_title, self.series_date, self.series_author,
+                                                      self.series_author_date)
                 else:
-                    return '%s, %s (%s)' % (self.series_title, self.series_date, self.series_author)
+                    result = '%s, %s (%s)' % (self.series_title, self.series_date, self.series_author)
             else:
-                return '%s, %s' % (self.series_title, self.series_date)
+                result = '%s, %s' % (self.series_title, self.series_date)
         else:
-            return '%s' % self.series_title
+            result = '%s' % self.series_title
+        return result
+
+    class Meta:
+        verbose_name_plural = 'Series'
+        ordering = ['series_title', 'series_date', 'series_author', 'series_author_date', 'series_author_identifier']
 
 
 class Subject(models.Model):
@@ -82,24 +92,28 @@ class Subject(models.Model):
     subject_added_subfield4 = models.CharField(max_length=255, blank=True, default='')
     subject_added_type = models.CharField(max_length=100)
 
+    def __str__(self):
+        result = ''
+        if self.subject_added_subfield4 != '':
+            result = '%s--%s--%s--%s--%s' % (self.subject_added, self. subject_added_subfield1,
+                                             self.subject_added_subfield2, self.subject_added_subfield3,
+                                             self.subject_added_subfield4)
+        elif self.subject_added_subfield3 != '':
+            result = '%s--%s--%s--%s' % (self.subject_added, self.subject_added_subfield1, self.subject_added_subfield2,
+                                         self.subject_added_subfield3)
+        elif self.subject_added_subfield2 != '':
+            result = '%s--%s--%s' % (self.subject_added, self.subject_added_subfield1, self.subject_added_subfield2)
+        elif self.subject_added_subfield1 != '':
+            result = '%s--%s' % (self.subject_added, self.subject_added_subfield1)
+        else:
+            result = '%s' % self.subject_added
+        return result
+
     class Meta:
         unique_together = ('subject_added', 'subject_added_subfield1', 'subject_added_subfield2',
                            'subject_added_subfield3', 'subject_added_subfield4', 'subject_added_type')
-
-    def __str__(self):
-        if self.subject_added_subfield4 != '':
-            return '%s--%s--%s--%s--%s' % (self.subject_added, self. subject_added_subfield1,
-                                           self.subject_added_subfield2, self.subject_added_subfield3,
-                                           self.subject_added_subfield4)
-        elif self.subject_added_subfield3 != '':
-            return '%s--%s--%s--%s' % (self.subject_added, self.subject_added_subfield1, self.subject_added_subfield2,
-                                       self.subject_added_subfield3)
-        elif self.subject_added_subfield2 != '':
-            return '%s--%s--%s' % (self.subject_added, self.subject_added_subfield1, self.subject_added_subfield2)
-        elif self.subject_added_subfield1 != '':
-            return '%s--%s' % (self.subject_added, self.subject_added_subfield1)
-        else:
-            return '%s' % self.subject_added
+        ordering = ['subject_added', 'subject_added_subfield1', 'subject_added_subfield2', 'subject_added_subfield3',
+                    'subject_added_subfield4']
 
 
 class AddedEntry(models.Model):
@@ -109,22 +123,25 @@ class AddedEntry(models.Model):
     added_entry_identifier = models.CharField(max_length=255, blank=True, default='')
     added_entry_type = models.CharField(max_length=255)
 
+    def __str__(self):
+        result = ''
+        if self.added_entry_date != '':
+            if self.added_entry_identifier != '':
+                result = '%s, %s (%s)' % (self.added_entry_name, self.added_entry_date, self.added_entry_identifier)
+            else:
+                result = '%s, %s' % (self.added_entry_name, self.added_entry_date)
+        else:
+            if self.added_entry_identifier != '':
+                result = '%s (%s)' % (self.added_entry_name, self.added_entry_identifier)
+            else:
+                result = '%s' % self.added_entry_name
+        return result
+
     class Meta:
         verbose_name = 'Added Entry'
         verbose_name_plural = 'Added Entries'
         unique_together = ('added_entry_name', 'added_entry_date', 'added_entry_identifier', 'added_entry_type')
-
-    def __str__(self):
-        if self.added_entry_date != '':
-            if self.added_entry_identifier != '':
-                return '%s, %s (%s)' % (self.added_entry_name, self.added_entry_date, self.added_entry_identifier)
-            else:
-                return '%s, %s' % (self.added_entry_name, self.added_entry_date)
-        else:
-            if self.added_entry_identifier != '':
-                return '%s (%s)' % (self.added_entry_name, self.added_entry_identifier)
-            else:
-                return '%s' % self.added_entry_name
+        ordering = ['added_entry_name', 'added_entry_date', 'added_entry_identifier']
 
 
 class Book(models.Model):
@@ -150,12 +167,18 @@ class Book(models.Model):
     book_note_contents = models.TextField(blank=True, default='')
     book_series_vol_num = models.CharField(max_length=50, blank=True, default='')
     work = models.ForeignKey(Work, related_name='books', on_delete=models.PROTECT)
-    series = models.ForeignKey(Series, related_name='books', blank=True, default='', on_delete=models.PROTECT)
-    subject = models.ForeignKey(Subject, related_name='books', blank=True, default='', on_delete=models.PROTECT)
-    added_entry = models.ForeignKey(AddedEntry, related_name='books', blank=True, default='', on_delete=models.PROTECT)
+    series = models.ForeignKey(Series, related_name='books', null=True, blank=True, default=None,
+                               on_delete=models.PROTECT)
+    subject = models.ForeignKey(Subject, related_name='books', null=True, blank=True, default=None,
+                                on_delete=models.PROTECT)
+    added_entry = models.ForeignKey(AddedEntry, related_name='books', null=True, blank=True, default=None,
+                                    on_delete=models.PROTECT)
 
     def __str__(self):
         return '%s' % self.work
+
+    class Meta:
+        ordering = ['work']
 
 
 class Location(models.Model):
@@ -165,14 +188,20 @@ class Location(models.Model):
     def __str__(self):
         return '%s' % self.location_name
 
+    class Meta:
+        ordering = ['location_name']
+
 
 class Collection(models.Model):
     collection_id = models.AutoField(primary_key=True)
     collection_name = models.CharField(max_length=255, unique=True)
-    location = models.ForeignKey(Location, related_name='collections', default='', on_delete=models.PROTECT)
+    location = models.ForeignKey(Location, related_name='collections', on_delete=models.PROTECT)
 
     def __str__(self):
         return '%s--%s' % (self.location, self.collection_name)
+
+    class Meta:
+        ordering = ['location', 'collection_name']
 
 
 class Holding(models.Model):
@@ -185,6 +214,9 @@ class Holding(models.Model):
 
     def __str__(self):
         return '%s (%s)' % (self.book, self.collection)
+
+    class Meta:
+        ordering = ['book', 'collection']
 
 
 class Item(models.Model):
@@ -204,13 +236,18 @@ class Item(models.Model):
     holding = models.ForeignKey(Holding, related_name='items', on_delete=models.PROTECT)
 
     def __str__(self):
+        result = ''
         if self.item_description != '':
             if self.item_copy != '':
-                return '%s--%s, copy %s' % (self.holding, self.item_description, self.item_copy)
+                result = '%s--%s, copy %s' % (self.holding, self.item_description, self.item_copy)
             else:
-                return '%s--%s' % (self.holding, self.item_description)
+                result = '%s--%s' % (self.holding, self.item_description)
         else:
             if self.item_copy != '':
-                return '%s, copy %s' % (self.holding, self.item_copy)
+                result = '%s, copy %s' % (self.holding, self.item_copy)
             else:
-                return '%s' % self.holding
+                result = '%s' % self.holding
+        return result
+
+    class Meta:
+        ordering = ['holding', 'item_description', 'item_copy']
