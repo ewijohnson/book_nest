14f0a998b41be81ab63b3d549d57b847dcc4fa1f
ewijohnson <ericwj2@illinois.edu>
2020-11-14 21:59:09 -0600

    added superuser and registered to admin.py

--- booknest/admin.py
+++ booknest/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import MainEntry, Work, Publication, PhysicalDescription, Series, \
+    Subject, AddedEntry, Book, Location, Collection, Holding, Item
 
-# Register your models here.
+
+admin.site.register(MainEntry)
+admin.site.register(Work)
+admin.site.register(Publication)
+admin.site.register(PhysicalDescription)
+admin.site.register(Series)
+admin.site.register(Subject)
+admin.site.register(AddedEntry)
+admin.site.register(Book)
+admin.site.register(Location)
+admin.site.register(Collection)
+admin.site.register(Holding)
+admin.site.register(Item)
